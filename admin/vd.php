1f1e299a61b6607e38a4428a2f39120e051fb666
giangthaihong <presidentboy987@gmail.com>
2018-11-14 18:07:46 +0700

    update

--- admin/vd.php
+++ admin/vd.php
@@ -1,12 +1,54 @@
-<div class="col-sm-12 col-md-7">
-	<div class="dataTables_paginate paging_simple_numbers" id="sampleTable_paginate">
-		<ul class="pagination">
-			<li class="paginate_button page-item previous" id="sampleTable_previous"><a href="#" aria-controls="sampleTable" data-dt-idx="0" tabindex="0" class="page-link">Previous</a>
-			</li>
-			<li class="paginate_button page-item "><a href="#" aria-controls="sampleTable" data-dt-idx="1" tabindex="0" class="page-link">1</a>
-			</li>
-			<li class="paginate_button page-item next disabled" id="sampleTable_next"><a href="#" aria-controls="sampleTable" data-dt-idx="7" tabindex="0" class="page-link">Next</a>
-			</li>
-		</ul>
+<?php
+$page_title = 'Thêm Bài Viết';
+include('header.php');
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<script src="ckeditor/ckeditor.js"></script>
+	<?php
+        if (isset($_POST['submit'])) {
+            //nếu submit rồi thì lấy các thông tin đã nhập
+            $content = $_POST['content'];
+				
+                //nếu đã đầy đủ thông tin cần thiết, tiến hành chèn vào CSDL
+                $sql = 'Insert into vd(content) values("'.$content.'")';
+                //thực thi câu lệnh SQL
+				if (mysqli_query($conn, $sql)) {  //biến $conn được khai báo trong file connectdb
+                    echo 'Đăng bài thành công!';
+                }
+		}
+			
+	?>
+	<form action="" method="post" enctype="multipart/form-data">
+    <div class="row">
+        <div class="col-md-12">
+          <div class="tile">
+            <h3 class="tile-title">Viết Bài</h3>
+            <div class="tile-body">
+                <div class="form-group">
+                  <label class="control-label">Nội dung</label>
+                  <textarea class="form-control" rows="4" placeholder="Enter your content" name="content"></textarea>
+                </div>
+            </div>
+            <div class="tile-footer">
+              <button class="btn btn-primary" type="submit" name="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i>Đăng Bài</button>&nbsp;&nbsp;&nbsp;
+			<a class="btn btn-secondary" href="#"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancel</a>
+            </div>
+          </div>
+        </div>
 	</div>
-</div>
+</form>
+	<script>
+	CKEDITOR.replace('content');
+	</script>
+	<?php
+include('footer.php');
+?>
+</body>
+</html>
--- vd.php
+++ vd.php
@@ -1,6 +1,55 @@
 <?php
-include 'admin/connectdb.php';
-$id = $_GET['id'];
-$current_page = isset($_GET['page']) ? $_GET['page'] : '1';
-echo $current_page;
-?> 
+$page_title = 'Thêm Bài Viết';
+include('header.php');
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<script src="ckeditor/ckeditor.js"></script>
+	<?php
+        if (isset($_POST['submit'])) {
+            //nếu submit rồi thì lấy các thông tin đã nhập
+            $content = $_POST['content'];
+				
+                //nếu đã đầy đủ thông tin cần thiết, tiến hành chèn vào CSDL
+                $sql = 'Insert into vd(content) values("'.$content.'")';
+                //thực thi câu lệnh SQL
+				$result = mysqli_query($conn, $sql1);
+				if (mysqli_query($conn, $sql)) {  //biến $conn được khai báo trong file connectdb
+                    echo 'Đăng bài thành công!';
+                }
+		}
+			
+	?>
+	<form action="" method="post" enctype="multipart/form-data">
+    <div class="row">
+        <div class="col-md-12">
+          <div class="tile">
+            <h3 class="tile-title">Viết Bài</h3>
+            <div class="tile-body">
+                <div class="form-group">
+                  <label class="control-label">Nội dung</label>
+                  <textarea class="form-control" rows="4" placeholder="Enter your content" name="content"></textarea>
+                </div>
+            </div>
+            <div class="tile-footer">
+              <button class="btn btn-primary" type="submit" name="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i>Đăng Bài</button>&nbsp;&nbsp;&nbsp;
+			<a class="btn btn-secondary" href="#"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancel</a>
+            </div>
+          </div>
+        </div>
+	</div>
+</form>
+	<script>
+	CKEDITOR.replace('content');
+	</script>
+	<?php
+include('footer.php');
+?>
+</body>
+</html>
